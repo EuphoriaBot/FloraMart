3395b3f5b1d7fd6142e07632042fce8334986ae7
EuphoriaBot <dimasfirja80@gmail.com>
2025-05-21 00:00:15 +0800

    Menu Utama Supplier

--- menu_supplier.h
+++ menu_supplier.h
@@ -1,22 +1,59 @@
 #ifndef MENU_SUPPLIER_H
 #define MENU_SUPPLIER_H
-
+#include <iostream>
+#include <filesystem>
 #include "menu_utilities.h"
+using namespace std;
+
+void SuplaiTanaman(DataUtama &data, InfoLogin infoLogin)
+{
+}
+
+void LihatLaporanTransaksi(DataUtama &data, InfoLogin infoLogin)
+{
+}
 
 void MenuUtamaSupplier(DataUtama &data, InfoLogin infoLogin, DataMenu dataMenu)
 {
     try
     {
-        // Menu Utama Supplier
+        RefreshDataUtama(data);
 
-        // Note: Disarankan untuk Refresh Data Utama di awal Fungsi
-        RefreshDataUtama(data);   
+        while (true)
+        {
+            cout << "\n=== MENU UTAMA SUPPLIER ===\n";
+            cout << "1. Suplai Tanaman\n";
+            cout << "2. Lihat Laporan Transaksi\n";
+            cout << "3. Logout\n";
+            cout << "Pilih menu: ";
+
+            string pilihan;
+            getline(cin, pilihan);
+
+            if (pilihan == "1")
+            {
+                SuplaiTanaman(data, infoLogin);
+            }
+            else if (pilihan == "2")
+            {
+                LihatLaporanTransaksi(data, infoLogin);
+            }
+            else if (pilihan == "3")
+            {
+                cout << "Logout berhasil.\n";
+                break;
+            }
+            else
+            {
+                cout << "Pilihan tidak valid. Coba lagi.\n";
+            }
+        }
     }
-    catch(invalid_argument& e)
+    catch (invalid_argument &e)
     {
         cout << e.what() << '\n';
     }
-    catch(exception& e)
+    catch (exception &e)
     {
         cout << e.what() << '\n';
     }

4a4a834686d26fa7887d75787a6e356dbe7ce7a2
Dnt <dante.engineer@gmail.com>
2025-05-20 22:12:00 +0700

    admin

--- menu_admin.h
+++ menu_admin.h
@@ -3,20 +3,136 @@
 
 #include "menu_utilities.h"
 
-void MenuUtamaAdmin(DataUtama &data, InfoLogin infoLogin, DataMenu dataMenu)
+void ManajemenKategori (DataUtama &data)
+{
+    RefreshDataUtama(data);
+    cout << "=== Manajemen Kategori ===" << endl;
+    cout << "1. Tambah Kategori" << endl;
+    cout << "2. Hapus Kategori" << endl;  
+    cout << "3. Edit Kategori" << endl;
+    cout << "4. Lihat Kategori" << endl;
+    cout << "Pilih menu:";
+
+    string pilihan;
+    getline(cin, pilihan);
+    if (pilihan == "1")
+    {
+        // TambahKategori(data);
+    }
+    else if (pilihan == "2")
+    {
+        // HapusKategori(data);
+    }
+    else if (pilihan == "3")
+    {
+        // EditKategori(data);
+    }
+    else if (pilihan == "4")
+    {
+        // LihatKategori(data);
+    }
+    else
+    {
+        cout << "Pilihan tidak valid!" << endl;
+    }
+}
+void ManajemenTanaman (DataUtama &data)
+{
+    RefreshDataUtama(data);
+    
+    cout << "=== Manajemen Tanaman ===" << endl;
+    cout << "1. Manajemen Kategori" << endl;
+    cout << "2. Validasi Status " << endl;
+    cout << "Pilih menu: ";
+
+    string pilihan;
+    getline(cin, pilihan);
+
+    if (pilihan == "1")
+    {
+        // TambahTanaman(data);
+    }
+    else if (pilihan == "2")
+    {
+        // HapusTanaman(data);
+    }
+    else if (pilihan == "3")
+    {
+        // EditTanaman(data);
+    }
+}
+
+void ManajemenSupplier (DataUtama &data)
+{
+    RefreshDataUtama(data);
+    
+    cout << "=== Manajemen Supplier ===" << endl;
+    cout << "1. Lihat Supplier" << endl;
+    cout << "2. Blokir Supplier" << endl;
+    cout << "3. Hapus Supplier" << endl;
+    cout << "Pilih menu: ";
+
+    string pilihan;
+    getline(cin, pilihan);
+
+    if (pilihan == "1")
+    {
+        // TambahSupplier(data);
+    }
+    else if (pilihan == "2")
+    {
+        // HapusSupplier(data);
+    }
+    else if (pilihan == "3")
+    {
+        // EditSupplier(data);
+    }
+}
+
+void MenuUtamaAdmin(DataUtama &data, InfoLogin &infoLogin, DataMenu &dataMenu)
 {
     try
     {
+        RefreshDataUtama(data);
+        
+        while (true)
+        {
+            cout << "\n=== Menu Utama Admin ===" << endl;
+            cout << "1. Manajemen Tanaman" << endl;
+            cout << "2. Manajemen Supplier" << endl;
+            cout << "3. Logout" << endl;
+            cout << "Pilih menu: ";
+
+            string pilihan;
+            getline(cin, pilihan);
+
+            if (pilihan == "1")
+            {
+                // MenuManajemenTanaman(data, infoLogin, dataMenu);
+            }
+            else if (pilihan == "2")
+            {
+                // MenuManajemenSupplier(data, infoLogin, dataMenu);
+            }
+            else if (pilihan == "3")
+            {
+                break; // Logout
+            }
+            else
+            {
+                cout << "Pilihan tidak valid! Silakan coba lagi." << endl;
+            }
+        }
         // Menu Utama Admin
 
         // Note: Disarankan untuk Refresh Data Utama di awal Fungsi
         RefreshDataUtama(data);
     }
-    catch(invalid_argument& e)
+    catch (invalid_argument &e)
     {
         cout << e.what() << '\n';
     }
-    catch(exception& e)
+    catch (exception &e)
     {
         cout << e.what() << '\n';
     }
@@ -31,11 +147,11 @@ void MenuManajemenTanaman(DataUtama &data, InfoLogin infoLogin, DataMenu dataMen
         // Note: Disarankan untuk Refresh Data Utama di awal Fungsi
         RefreshDataUtama(data);
     }
-    catch(invalid_argument& e)
+    catch (invalid_argument &e)
     {
         cout << e.what() << '\n';
     }
-    catch(exception& e)
+    catch (exception &e)
     {
         cout << e.what() << '\n';
     }
--- menu_pembeli.h
+++ menu_pembeli.h
@@ -8,14 +8,12 @@ using namespace std;
 void loadDataTanaman(DataUtama &data)
 {
     json tanamanJson;
-    ReadJson(tanamanJson, data.sizeDataTanaman, "data_tanaman.json");
 
     for (int i = 0; i < data.sizeDataTanaman; i++)
     {
         data.dataTanaman[i].id = tanamanJson[i]["id"];
         data.dataTanaman[i].namaTanaman = tanamanJson[i]["namaTanaman"];
         data.dataTanaman[i].harga = tanamanJson[i]["harga"];
-        data.dataTanaman[i].stok = tanamanJson[i]["stok"];
     }
 }
 
@@ -32,8 +30,21 @@ void DaftarTanaman(DataUtama &data)
     cout << "ID\tNama\t\tHarga";
     for (int i = 0; i < data.sizeDataTanaman; i++)
     {
-        cout << data.dataTanaman[i].id << "\t" << data.dataTanaman[i].namaTanaman << "\t\t" << data.dataTanaman[i].harga << "\t" << endl;
+        cout << data.dataTanaman[i].id << "" << data.dataTanaman[i].namaTanaman << "" << data.dataTanaman[i].harga << "" << endl;
     }
+}
+
+int LinearSearch(DataUtama &data, string &key)
+{
+    int n = data.sizeDataTanaman;
+    for (int i = 0; i < n; i++)
+    {
+        if (data.dataTanaman[i].namaTanaman == key)
+        {
+            return i;
+        }
+    }
+    return -1;
 }
 
 void MenuUtamaPembeli(DataUtama &data, InfoLogin &infoLogin, DataMenu &dataMenu)
@@ -55,11 +66,35 @@ void MenuUtamaPembeli(DataUtama &data, InfoLogin &infoLogin, DataMenu &dataMenu)
 
             if (pilihan == "1")
             {
-                loadDataTanaman(data);
+                RefreshDataUtama(data);
                 DaftarTanaman(data);
             }
             else if (pilihan == "2")
             {
+                RefreshDataUtama(data);
+                cout << "Masukkan nama tanaman: ";
+                string key;
+                getline(cin, key);
+                int Index = LinearSearch(data, key);
+                if (Index != -1)
+                {
+                    auto &dataTanaman = data.dataTanaman[Index];
+                    cout << "=== Detail Tanaman ===" << endl;
+                    cout << "ID               : " << dataTanaman.id << endl;
+                    cout << "Nama             : " << dataTanaman.namaTanaman << endl;
+                    cout << "Harga            : " << dataTanaman.harga << endl;
+                    cout << "Stok             : " << dataTanaman.stok << endl;
+
+                    auto &dataKategori = data.dataKategori[Index];
+                    cout << "Kategori         : " << dataKategori.namaKategori << endl;
+                    cout << "HIDUP JOKOWII    : " << dataKategori.minSuhu << endl;
+                    cout << "Max Suhu         : " << dataKategori.maxSuhu << endl;
+                    cout << "Media Tanam      : " << dataKategori.mediaTanam << endl;
+                }
+                else
+                {
+                    cout << "Tanaman dengan nama '" << key << "' tidak ditemukan." << endl;
+                }
             }
             else if (pilihan == "3")
             {
@@ -68,7 +103,7 @@ void MenuUtamaPembeli(DataUtama &data, InfoLogin &infoLogin, DataMenu &dataMenu)
             }
             else
             {
-                cout << "Pilihan tidak valid." << endl;
+                cout << "Pilihan tidak valid" << endl;
             }
         }
     }
